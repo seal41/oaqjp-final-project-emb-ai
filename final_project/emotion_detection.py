ae186285a32fbc9bf4e77d8f491f99b002577924
seal41 <awa1855@gmail.com>
2025-08-04 11:55:52 -0400

    after 3   commit

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,29 +1,29 @@
 import requests
 import json
 
-def sentiment_analyzer(text_to_analyse):
-    # URL of the sentiment analysis service
+def emotion_detector(text_to_analyse):
+    # URL of the emotion detection  service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyse } }
 
-    # Custom header specifying the model ID for the sentiment analysis service
+    # Custom header specifying the model ID for the emotion detection service
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-    # Sending a POST request to the sentiment analysis API
+    # Sending a POST request to the emotion detection API
     response = requests.post(url, json=myobj, headers=header)
 
     # Parsing the JSON response from the API
     data = json.loads(response.text)
 
-    # Emotionen extrahieren
+    # Emotionen extraction
     emotions = data['emotionPredictions'][0]['emotion']
     
-    # Dominante Emotion finden
+    # find dominant emotion 
     dominant_emotion = max(emotions, key=emotions.get)
     
-    # Result Dictionary erstellen
+    # create Result Dictionary
     result = {
         'anger': emotions['anger'],
         'disgust': emotions['disgust'],
@@ -33,6 +33,6 @@ def sentiment_analyzer(text_to_analyse):
         'dominant_emotion': dominant_emotion
     }
 
-    # Returning a dictionary containing sentiment analysis results
+    # Returning a dictionary containing emotion detection  results
     return result
     
